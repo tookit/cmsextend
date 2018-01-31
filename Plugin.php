35111b1bcc2bd7fb299f73d145c36ef3356079ea
Micahel Wang <xiangyu.wang@neptunetechnologieschina.com>
2018-01-31 12:20:06 +0800

    Extend backend brandsettings

--- Plugin.php
+++ Plugin.php
@@ -1,5 +1,6 @@
 <?php namespace Socklab\Cmsextend;
 
+use Backend\Widgets\Form;
 use System\Classes\PluginBase;
 use Illuminate\Support\Facades\Event;
 
@@ -15,33 +16,81 @@ class Plugin extends PluginBase
 
     public function register()
     {
-        Event::listen('backend.form.extendFields', function($widget) {
-            if (!$widget->model instanceof \Cms\Classes\Page) return;
+        Event::listen('backend.form.extendFields', function(Form $widget) {
+            if ($widget->model instanceof \Cms\Classes\Page){
+                $widget->addFields([
+                    'settings[meta_keywords]' => [
+                        'label' => 'Meta Keywords',
+                        'tab' => 'cms::lang.editor.meta',
+                        'type' => 'textarea',
+                        'size' => 'tiny',
+                        'span' => 'full',
+                    ],
+                    'settings[show_in_nav]' => [
+                        'label' => 'In Navigation',
+                        'tab' => 'cms::lang.editor.meta',
+                        'type' => 'checkbox',
+                        'size' => 'tiny',
+                        'span' => 'right',
+                    ],
+                    'settings[order_in_nav]' => [
+                        'label' => 'Order In Navigation',
+                        'tab' => 'cms::lang.editor.meta',
+                        'type' => 'number',
+                        'default' => '1',
+                        'size' => 'tiny',
+                        'span' => 'left',
+                    ],
+                ], 'primary');
+            }elseif ($widget->model instanceof \Backend\Models\BrandSetting){
+                $widget->addFields([
+                    'social[twitter]' => [
+                        'label' => 'Twitter',
+                        'tab' => 'Social',
+                        'type' => 'text',
+                        'span' => 'full',
+                    ],
+                    'social[facebook]' => [
+                        'label' => 'Facebook',
+                        'tab' => 'Social',
+                        'type' => 'text',
+                        'span' => 'full',
+                    ],
+                    'social[youtube]' => [
+                        'label' => 'Youtube',
+                        'tab' => 'Social',
+                        'type' => 'text',
+                        'span' => 'full',
+                    ],
+                    'social[googleplus]' => [
+                        'label' => 'Facebook',
+                        'tab' => 'Social',
+                        'type' => 'text',
+                        'span' => 'full',
+                    ],
+                    'social[qq]' => [
+                        'label' => 'QQ',
+                        'tab' => 'Social',
+                        'type' => 'text',
+                        'span' => 'full',
+                    ],
+                    'social[skype]' => [
+                        'label' => 'Skype',
+                        'tab' => 'Social',
+                        'type' => 'text',
+                        'span' => 'full',
+                    ],
+                    'social[github]' => [
+                        'label' => 'Github',
+                        'tab' => 'Social',
+                        'type' => 'text',
+                        'span' => 'full',
+                    ],
 
-            $widget->addFields([
-                'settings[meta_keywords]' => [
-                    'label' => 'Meta Keywords',
-                    'tab' => 'cms::lang.editor.meta',
-                    'type' => 'textarea',
-                    'size' => 'tiny',
-                    'span' => 'full',
-                ],
-                'settings[show_in_nav]' => [
-                    'label' => 'In Navigation',
-                    'tab' => 'cms::lang.editor.meta',
-                    'type' => 'checkbox',
-                    'size' => 'tiny',
-                    'span' => 'right',
-                ],
-                'settings[order_in_nav]' => [
-                    'label' => 'Order In Navigation',
-                    'tab' => 'cms::lang.editor.meta',
-                    'type' => 'number',
-                    'default' => '1',
-                    'size' => 'tiny',
-                    'span' => 'left',
-                ],
-            ], 'primary');
+                ], 'primary');
+            }
+
+
         });
     }
 }
